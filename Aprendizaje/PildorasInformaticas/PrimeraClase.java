8a0e0ddc11279cf41b349cd5925db3257969b0eb
Xaviku91 <xavierquesada91@gmail.com>
2023-02-14 10:46:22 +0100

    nuevo

---
+++ Aprendizaje/PildorasInformaticas/PrimeraClase.java
@@ -0,0 +1,6 @@
+package Aprendizaje.PildorasInformaticas;
+public class PrimeraClase {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
---
+++ Aprendizaje/PildorasInformaticas/variables.java
@@ -0,0 +1,55 @@
+package Aprendizaje.PildorasInformaticas;
+
+public class variables {
+    // Esto es un comentario
+    public static void main(String[] args){
+
+        // Declaración de variables (byte)
+        byte edad;
+        edad=31;
+
+        System.out.println("Mi edad es: " + edad);
+
+        // Declaración de variables (int)
+        int edad2=32;
+        System.out.println("La edad de Pablo es: " + edad2);
+
+        // Declaración de variables (short)
+        short edad3=33;
+        System.out.println("La edad de Juan es: " + edad3);
+
+        // Declaración de variables (long)
+        long edad4=34;
+        System.out.println("La edad de Pedro es: " + edad4);
+
+        // Declaración de variables (float) 
+        float altura=1.75f;
+        System.out.println("La altura de Juan es: " + altura);
+
+        // Declaración de variables (double)
+        double altura2=1.76;
+        System.out.println("La altura de Pedro es: " + altura2);
+
+        // Declaración de variables (char) - Caracteres
+        char inicial='J';
+        System.out.println("La inicial de Juan es: " + inicial);
+
+        // Declaración de variables (boolean)
+        boolean casado=false;
+        System.out.println("¿Está casado Juan? " + casado);
+
+        // Declaración de variables (String) - Cadena de caracteres
+        String ciudad="Sabadell";
+        System.out.println("Juan vive en: " + ciudad);
+
+        // Cambiar el valor de una variable 
+        edad=28;
+        System.out.println("Ahora mi edad es: " + edad);
+
+        // Declaración de variables (final) - No se puede cambiar el valor
+        final float pi=3.1416f;
+        System.out.println("El valor de pi es: " + pi);
+        // pi=3.315f; // Esto no se puede hacer porque es una variable final y porque pi siempre es 3.1416
+    }
+
+}
